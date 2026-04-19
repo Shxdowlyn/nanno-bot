5e07acc366584f23717afc576278f3a9c713c747
shxdowlynnie <thirdeyebikes@gmail.com>
2026-04-19 00:25:31 -0300

    Actualizar ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,18 +1,7 @@
 export default {
-  command: ['ping', 'p'],
-  category: 'tools',
+  command: ['ping'],
 
   run: async (m, { conn }) => {
-    const start = Date.now()
-
-    const msg = await conn.sendMessage(m.chat, {
-      text: 'Pinging...'
-    }, { quoted: m })
-
-    const end = Date.now()
-
-    await conn.sendMessage(m.chat, {
-      text: `🏓 Pong!\n⚡ Speed: ${end - start} ms`
-    }, { quoted: msg })
+    await conn.sendMessage(m.chat, { text: '🏓 Pong!' }, { quoted: m })
   }
 }
